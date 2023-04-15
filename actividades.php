c5ff9c94058baf9b4c59a17f20932b62fd5b271f
Nojands <luisjtl86@gmail.com>
2023-04-15 11:40:15 -0600

    actividades style and html added, tried to create a php page for information, working in progress

---
+++ actividades.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bienvenido a FUELOGGER</title>
+    <link rel="stylesheet" href="styleavtividades.css">
+</head>
+<body>
+    <h1>Bienvenido, <?php echo $_POST["nombre"]; ?>!</h1>
+    <p>Gracias por iniciar sesión en FUELOGGER.</p>
+    <a href="index.html"><button>Cerrar Sesión</button></a>
+</body>
+</html>
